d6ae43b495fbdbe27fc43c76b8ee11cdf03b4770
Miguel Angel <miguel_qdoq@hotmail.com>
2024-10-21 16:23:23 -0600

    primer commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let nombre = "Diego"; 
+
+console.log(nombre); 
+
+ 
+
+let nota = 92; 
+
+if (nota>=90) { 
+
+    console.log("Excelente"); 
+
+} else if (nota>=75) { 
+
+    console.log("bien") 
+
+} else if (nota>=60) { 
+
+    console.log("Suficiente") 
+
+} else if (nota <59) { 
+
+    console.log("El estudiante no aprueba") 
+
+} 
+
+console.log(nota); 
